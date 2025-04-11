035caf6c63044b83f256bbd668025ef2cef283ae
andriichumakov <andrii.chumakov@student.nhlstenden.com>
2025-04-11 23:31:00 +0200

    hello world from avalonia

---
+++ SkyLens/SkyLens2/SkyLens2/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SkyLens2.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ SkyLens/SkyLens2/SkyLens2/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace SkyLens2.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
