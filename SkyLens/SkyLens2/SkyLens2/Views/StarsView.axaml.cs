4a4f53f820e9a6fd27d4b8b183894c95328cc787
anastasialukanova <anastasia.lukanova@student.nhlstenden.com>
2025-04-12 18:38:27 +0200

    Created initial code for the home page

---
+++ SkyLens/SkyLens2/SkyLens2/Views/StarsView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SkyLens2.Views;
+
+public partial class StarsView : UserControl
+{
+    public StarsView()
+    {
+        InitializeComponent();
+    }
+}
