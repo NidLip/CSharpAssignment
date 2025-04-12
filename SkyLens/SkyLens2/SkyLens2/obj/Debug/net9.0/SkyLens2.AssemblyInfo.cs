161d6d18380b6caaaab0f98c7c1995f137c3999d
Nikolay Minins <nikolay.minins@student.nhlstenden.com>
2025-04-12 14:25:10 +0200

    Pushed unstaged files

--- SkyLens/SkyLens2/SkyLens2/obj/Debug/net9.0/SkyLens2.AssemblyInfo.cs
+++ SkyLens/SkyLens2/SkyLens2/obj/Debug/net9.0/SkyLens2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SkyLens2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+035caf6c63044b83f256bbd668025ef2cef283ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+898768818b2f2b93db59fe33968c5d0ce80b657a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SkyLens2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SkyLens2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
