b685803b930751482157437c1d1f6ece4f7fbc4d
anastasialukanova <anastasia.lukanova@student.nhlstenden.com>
2025-04-13 18:36:52 +0200

    Fixed styling and made home page dynamic

--- SkyLens/SkyLens2/SkyLens2/Views/MainWindow.axaml.cs
+++ SkyLens/SkyLens2/SkyLens2/Views/MainWindow.axaml.cs
@@ -1,66 +1,80 @@
-using Avalonia.Controls;
-using Avalonia.Interactivity;
-using Avalonia.Threading;
-using System;
-using System.Timers;
+    using Avalonia.Controls;
+    using Avalonia.Interactivity;
+    using Avalonia.Threading;
+    using System;
+    using System.Timers;
+    using SkyLens2.ViewModels;
 
-namespace SkyLens2.Views
-{
-    public partial class MainWindow : Window
+    namespace SkyLens2.Views
     {
-        private Timer _timer;
-
-        public MainWindow()
+        public partial class MainWindow : Window
         {
-            InitializeComponent();
-            StartDateTimeUpdater();
+            public static MainWindow Instance { get; private set; }
+            private Timer _timer;
 
-            // Optionally load homepage content here if you're navigating
-            // MainContent.Content = new HomeView(); 
-        }
-
-        private void StartDateTimeUpdater()
-        {
-            _timer = new Timer(60000); // update every minute
-            _timer.Elapsed += (s, e) => UpdateDateTime();
-            _timer.Start();
-            UpdateDateTime();
-        }
-
-        private void UpdateDateTime()
-        {
-            Dispatcher.UIThread.Post(() =>
+            public MainWindow()
             {
-                var now = DateTime.Now;
-                var formatted = now.ToString("yyyy/MM/dd hh:mm tt");
-                DateTimeText.Text = formatted;
-            });
-        }
+                Instance = this;
+                
+                InitializeComponent();
+                StartDateTimeUpdater();
+                
+                MainContent.Content = new HomeView();
+                
+            }
 
-        private void OnExploreSkyClick(object? sender, RoutedEventArgs e)
-        {
-            // TODO: Implement logic
-        }
+            private void StartDateTimeUpdater()
+            {
+                _timer = new Timer(60000); // update every minute
+                _timer.Elapsed += (s, e) => UpdateDateTime();
+                _timer.Start();
+                UpdateDateTime();
+            }
 
-        private void OnSeeStarChartClick(object? sender, RoutedEventArgs e)
-        {
-            // TODO: Implement logic
-        }
+            private void UpdateDateTime()
+            {
+                Dispatcher.UIThread.Post(() =>
+                {
+                    var now = DateTime.Now;
+                    var formatted = now.ToString("yyyy/MM/dd hh:mm tt");
+                    DateTimeText.Text = formatted;
+                });
+            }
+            
+            private void OnNavigateHome(object? sender, RoutedEventArgs e)
+            {
+                MainContent.Content = new HomeView();
+            }
 
-        private void OnNavigateConstellations(object? sender, RoutedEventArgs e)
-        {
-            // Only works if navigation and ConstellationsView is setup
-            // MainContent.Content = new ConstellationsView();
-        }
+            private void OnNavigateWeather(object? sender, RoutedEventArgs e)
+            {
+                
+            }
 
-        private void OnNavigatePlanets(object? sender, RoutedEventArgs e)
-        {
-            // MainContent.Content = new PlanetsView();
-        }
+            private void OnExploreSkyClick(object? sender, RoutedEventArgs e)
+            {
+                // TODO: Implement logic
+            }
 
-        private void OnNavigateStars(object? sender, RoutedEventArgs e)
-        {
-            MainContent.Content = new StarsView();
+            private void OnSeeStarChartClick(object? sender, RoutedEventArgs e)
+            {
+                // TODO: Implement logic
+            }
+
+            private void OnNavigateConstellations(object? sender, RoutedEventArgs e)
+            {
+                // Only works if navigation and ConstellationsView is setup
+                MainContent.Content = new ConstellationsView();
+            }
+
+            private void OnNavigatePlanets(object? sender, RoutedEventArgs e)
+            {
+                MainContent.Content = new PlanetsView();
+            }
+
+            private void OnNavigateStars(object? sender, RoutedEventArgs e)
+            {
+                MainContent.Content = new StarsView();
+            }
         }
     }
-}
--- SkyLens/SkyLens2/SkyLens2/obj/Debug/net9.0/SkyLens2.AssemblyInfo.cs
+++ SkyLens/SkyLens2/SkyLens2/obj/Debug/net9.0/SkyLens2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SkyLens2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a4f53f820e9a6fd27d4b8b183894c95328cc787")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5414834ed55f37b17caf61b7e809ff182d320c0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("SkyLens2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SkyLens2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
