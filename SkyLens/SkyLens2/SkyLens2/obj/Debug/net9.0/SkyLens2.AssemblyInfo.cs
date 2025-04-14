6e16f434a0ec5e7dae417f46ff02f93d73b7f34f
anastasialukanova <anastasia.lukanova@student.nhlstenden.com>
2025-04-14 22:43:19 +0200

    deleted bin and obj folders

--- SkyLens/SkyLens2/SkyLens2/obj/Debug/net9.0/SkyLens2.AssemblyInfo.cs
+++ SkyLens/SkyLens2/SkyLens2/obj/Debug/net9.0/SkyLens2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SkyLens2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6816ae9ffb85d6153999c51a7657b9e776193430")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07dd9c90eecb39ee4fb4c02601dd55cea8e741a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SkyLens2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SkyLens2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
